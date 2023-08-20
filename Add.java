689816d61529a6a80c18a011ce065f88c2d20ed2
GreeshmaAneesh <greeshma1997ks@gmail.com>
2023-08-20 11:38:27 +0530

    pgm

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add{
+public static void main(String arg[]){
+int a=5;
+int b=10;
+int sum=a+b;
+if (sum%10==0){
+System.out.println("Divisible by 10");
+}
+else
+{
+System.out.println("Not divisible by 10");
+}
+}
+}
